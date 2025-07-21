0fefc26030e487aa963b95e6372d765c67858dd3
z58occc <a0981583030@gmail.com>
2025-07-21 22:40:23 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/homework-w3-w4/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
